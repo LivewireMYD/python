98d4826183e90f9c140c19b27fedd6a5538ebe8f
LivewireMYD <157876005+LivewireMYD@users.noreply.github.com>
2024-12-06 16:46:34 +0530

    Add files via upload

---
+++ starting.py
@@ -0,0 +1,10 @@
+print("Hello World")
+print("hello"+"world")
+print("hello","world")
+
+print("hello","world","from","python")
+print("hello","world","from","python",sep=" * ")
+print("hello","world","from","python",sep="\n")
+
+print("hello",end="\t")
+print("world")
